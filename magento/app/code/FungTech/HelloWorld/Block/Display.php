91a7d4a878d3b3bde3867f721ccd768ab3f295d5
Lam Hoi Fung <lhf516@gmail.com>
2024-04-29 00:34:29 +0800

    Added view, layout, template for tutorial

---
+++ magento/app/code/FungTech/HelloWorld/Block/Display.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FungTech\HelloWorld\Block;
+
+class Display extends \Magento\Framework\View\Element\Template
+{
+  protected $_postFactory;
+
+  public function __construct(
+    \Magento\Framework\View\Element\Template\Context $context,
+    \FungTech\HelloWorld\Model\PostFactory $postFactory
+  ) {
+    $this->_postFactory = $postFactory;
+    parent::__construct($context);
+  }
+
+  public function sayHello()
+  {
+    return __('Hello World');
+  }
+
+  public function getPostCollection()
+  {
+    $post = $this->_postFactory->create();
+    return $post->getCollection();
+  }
+}
